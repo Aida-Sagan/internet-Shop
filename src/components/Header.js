3f9eb5466936bc9e9ad8a9dd5e880177f54db609
Aida <aida2001@mail.ru>
2024-03-02 00:29:59 +0600

    added counting the amount of items

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,17 @@ import {FaShoppingCart} from "react-icons/fa";
 import Order from "./Order";
 
 const showOrders = (props) =>{
+    let summa = 0;
+    props.orders.forEach(el => summa += Number.parseFloat(el.price));
+
     return (
         <div>
-            {props.orders.map((el) => (
-                <Order key={el.id} item={el} onDelete={props.onDelete}/>
+
+            {
+                props.orders.map((el) => (
+                <Order key={el.id} item={el} onDelete={props.onDelete} />
             ))}
+            <h2>Итого: {new Intl.NumberFormat().format(summa)} $</h2>
         </div>
     )
 }
@@ -38,7 +44,9 @@ export default function Header(props){
                         {props.orders.length > 0 ?
                         showOrders(props) : showNothing()}
                     </div>
+
                 )}
+
             </div>
             <div className='presentation'></div>
         </header>
--- src/components/Order.js
+++ src/components/Order.js
@@ -10,7 +10,8 @@ export default class Order extends Component{
                 <h2>{this.props.item.title}</h2>
                 <p>{this.props.item.desc}</p>
                 <b>{this.props.item.price}$</b>
-                <FaTrash className='deleteIcon' onClick={() => this.props.onDelete(this.props.item.id)}/>
+                <FaTrash className='deleteIcon' onClick={() => this.props.onDelete(this.props.item.id)}
+                />
             </div>
         )
     }
