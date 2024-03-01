8246f74bbc6d3143dc8c35355b12381c3b28bf91
Aida <aida2001@mail.ru>
2024-03-02 01:58:42 +0600

    заменила хранение товаров на sessionStorage, hooks

--- src/App.js
+++ src/App.js
@@ -3,97 +3,96 @@ import Footer from "./components/Footer";
 import Items from "./components/Items";
 import Category from "./components/Category";
 
-import {Component} from "react";
+import {Component, useEffect, useState} from "react";
 
-class App extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            //где мы храним фотки вместо БД или сессион сторэдж
-            items: [
-                {
-                    id:1,
-                    title: 'Kitchen',
-                    img: '2.jpg',
-                    description: '',
-                    price: '1200',
-                    category: 'chairs'
-                },
-                {
-                    id:2,
-                    title: 'Chair',
-                    img: '1.jpg',
-                    description: '',
-                    price: '2500',
-                    category: 'chairs'
-                },
-                {
-                    id:3,
-                    title: 'Chair',
-                    img: '4.jpg',
-                    description: '',
-                    price: '1370',
-                    category: 'chairs'
-                },
-                {
-                    id:4,
-                    title: 'Sofa',
-                    img: '3.jpg',
-                    description: '',
-                    price: '560',
-                    category: 'sofa'
-                },
-            ],
-            // где мы храним товары в корзине
-            orders: [],
+function App() {
+    const itemsObj = [
+        {
+            id:1,
+            title: 'Kitchen',
+            img: '2.jpg',
+            description: '',
+            price: '1200',
+            category: 'chairs'
+        },
+        {
+            id:2,
+            title: 'Chair',
+            img: '1.jpg',
+            description: '',
+            price: '2500',
+            category: 'chairs'
+        },
+        {
+            id:3,
+            title: 'Chair',
+            img: '4.jpg',
+            description: '',
+            price: '1370',
+            category: 'chairs'
+        },
+        {
+            id:4,
+            title: 'Sofa',
+            img: '3.jpg',
+            description: '',
+            price: '560',
+            category: 'sofa'
+        },
+    ]
+    const itemsJson = JSON.stringify(itemsObj);
 
-            currentCategories: [],
-        }
-//в методе могу взаимодействовать с состояниями
-        this.currentCategories = this.state.items;
-        this.addToOrder = this.addToOrder.bind(this);
-        this.deleteOrder = this.deleteOrder.bind(this);
-        this.chooseCategory = this.chooseCategory.bind(this);
-    }
+    const [items, setItems] = useState(itemsObj);
+    const [orders, setOrders] = useState([]);
+    const [currentCategories, setCurrentCategories] = useState([]);
 
-    addToOrder(item) {
+        useEffect(() => {
+            const storedItems = sessionStorage.getItem('items');
+            if (storedItems) {
+                setItems(JSON.parse(storedItems));
+            }
+
+        }, []);
+
+        useEffect(() => {
+            sessionStorage.setItem("items", JSON.stringify(items));
+        },[items]);
+
+
+    const addToOrder = (item) => {
         let isInArray = false;
-        this.state.orders.forEach((elem) => {
+        orders.forEach((elem) => {
             if (elem.id === item.id) {
                 isInArray = true;
             }
         })
         if(!isInArray) {
-            this.setState( {orders: [...this.state.orders, item]}, () => {
-                console.log(this.state.orders)
-            })
+            setOrders( [...this.state.orders, item]);
         }
     }
 
-    deleteOrder(id) {
-        this.setState({orders: this.state.orders.filter( el => el.id !== id )})
+    const deleteOrder = (id) => {
+        setOrders(orders.filter( el => el.id !== id ))
     }
 
-    chooseCategory(category) {
+    const chooseCategory = (category)=> {
         if (category === 'all') {
-            this.setState({currentCategories: this.state.items})
+            setCurrentCategories(items);
             return
         }
 
-        this.setState({currentCategories: this.state.items.filter(el => el.category === category)})
+        setCurrentCategories( items.filter(el => el.category === category))
     }
 
-    render()    {
-        return (
-            <div className='wrapper'>
-                <Header orders={this.state.orders} onDelete={this.deleteOrder}></Header>
-                <Category chooseCategory={this.chooseCategory}/>
-                <Items items={this.state.currentCategories} onAdd={this.addToOrder}/>
-                <Footer></Footer>
-            </div>
-        );
-    }
 
+    return (
+        <div className="wrapper">
+            <Header orders={orders} onDelete={deleteOrder}></Header>
+            <Category chooseCategory={chooseCategory}/>
+            <Items items={currentCategories} onAdd={addToOrder}/>
+            <Footer></Footer>
+        </div>
+    );
 
 }
 
